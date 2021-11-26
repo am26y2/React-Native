c3ff37dabcfd1ad392a1c5f54499f48f9a4447aa
am26y2 <amitraj9046@gmail.com>
2021-11-26 13:21:57 +0530

    Menu

--- components/Menu.js
+++ components/Menu.js
@@ -26,7 +26,7 @@ export default function Menu() {
     >
       <Login/>
       <Box>
-        <HStack bg="indigo.600" alignItems="center" safeAreaBottom shadow={6}>
+        <HStack  alignItems="center" safeAreaBottom shadow={6}>
           <Pressable
             opacity={selected === 0 ? 1 : 0.5}
             py="3"
@@ -115,14 +115,14 @@ export default function Menu() {
             opacity={selected === 3 ? 1 : 0.5}
             py="2"
             flex={1}
-            onPress={() => setSelected(3)}
+            onPress={() => setSelected(4)}
           >
             <Center>
               <Icon
                 mb="1"
                 as={
                   <MaterialCommunityIcons
-                    name={selected === 3 ? "account" : "account-outline"}
+                    name={selected === 4 ? "account" : "account-outline"}
                   />
                 }
                 color="#999999"
