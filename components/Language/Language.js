1adecc2a24474ca21f3bc33195dac271ec893078
am26y2 <amitraj9046@gmail.com>
2021-11-30 14:48:42 +0530

    Preferences

--- components/Language/Language.js
+++ components/Language/Language.js
@@ -1,47 +1,40 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 import Constants from "expo-constants";
 
 export default function Language() {
-
-  const [ pickerValue,setPickerValue ] = useState('en')
+  const [pickerValue, setPickerValue] = useState("en");
+  const languageList = [
+    { label: "English", value: "en" },
+    { label: "Deutsch", value: "de" },
+    { label: "French", value: "fr" },
+  ];
   return (
     <View style={styles.container}>
-      <Text style={styles.paragraph}>React native dropdown picker</Text>
-      {/* <DropDownPicker
-      style={{zIndex: 1000, elevation: 1000 }}
-        items={[
-          { label: "English", value: "en" },
-          { label: "Deutsch", value: "de" },
-          { label: "French", value: "fr" },
-        ]}
-        defaultIndex={0}
-        containerStyle={{ height: 40 }}
-        onChangeItem={(item) => console.log(item.label, item.value)}
-      /> */}
-      <Picker selectedValue={pickerValue} onValueChange={(itemValue) => setPickerValue(itemValue)}>
-        <Picker.Item label="English" value="en" />
-        <Picker.Item label="Deutsch" value="de" />
-        <Picker.Item label="French" value="fr" />
-      </Picker>
+      <Text style={styles.paragraph}>Choose your Language</Text>
+      <View style={{elevation:1,padding:10,borderRadius:1}}>
+        <Picker
+          selectedValue={pickerValue}
+          onValueChange={(itemValue) => setPickerValue(itemValue)}
+        >
+          {languageList.map((item, index) => (
+            <Picker.Item key={index} label={item.label} value={item.value} />
+          ))}
+        </Picker>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: "#ecf0f1",
     padding: 8,
-    height: 400
   },
   paragraph: {
-    margin: 24,
+    marginBottom: 24,
     fontSize: 18,
     fontWeight: "bold",
-    textAlign: "center",
+    // textAlign: "center",
   },
 });
---
+++ components/SentNotification.js
@@ -0,0 +1,54 @@
+import * as React from "react";
+import { Checkbox } from "react-native-paper";
+import { View, Text } from "react-native";
+
+const SentNotification = () => {
+  const [data, SetData] = React.useState([
+    { id: 1, txt: "SMS", isChecked: false },
+    { id: 2, txt: "Whatsapp", isChecked: false },
+    { id: 3, txt: "Call", isChecked: false },
+    { id: 4, txt: "Email", isChecked: false },
+    { id: 5, txt: "Do Not Disturb", isChecked: false },
+  ]);
+  function setSelection(el) {
+    console.log(el.target.name);
+  }
+
+  return (
+    <View style={{marginTop:20}}>
+      <Text style={{ fontSize: 20}}>Send Notifications only on</Text>
+      <View
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          marginVertical:10,
+        }}
+      >
+        {data.map((item, index) => (
+          <View
+            key={index}
+            style={{
+              margin: 0,
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              flexGrow:2,
+              justifyContent:"flex-start"
+            }}
+          >
+            <Checkbox
+              status={item.isChecked}
+              onPress={(item) => setSelection(item)}
+            />
+            <Text style={{ fontSize: 20 }}>{item.txt}</Text>
+          </View>
+        ))}
+      </View>
+    </View>
+  );
+};
+
+export default SentNotification;
