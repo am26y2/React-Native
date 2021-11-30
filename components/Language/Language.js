8c69be7ba4e29324e2835cd6ed61340884936245
Unmilon Borthakur <unmilon.7122@gmail.com>
2021-11-30 12:36:48 +0530

    dropdown fix

--- components/Language/Language.js
+++ components/Language/Language.js
@@ -1,9 +1,11 @@
-import * as React from "react";
+import React, {useState} from "react";
 import { Text, View, StyleSheet } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 import Constants from "expo-constants";
 
 export default function Language() {
+
+  const [ pickerValue,setPickerValue ] = useState('en')
   return (
     <View style={styles.container}>
       <Text style={styles.paragraph}>React native dropdown picker</Text>
@@ -18,7 +20,7 @@ export default function Language() {
         containerStyle={{ height: 40 }}
         onChangeItem={(item) => console.log(item.label, item.value)}
       /> */}
-      <Picker>
+      <Picker selectedValue={pickerValue} onValueChange={(itemValue) => setPickerValue(itemValue)}>
         <Picker.Item label="English" value="en" />
         <Picker.Item label="Deutsch" value="de" />
         <Picker.Item label="French" value="fr" />
